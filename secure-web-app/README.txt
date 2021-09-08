19f88fcc0cece6d2c5e5011fe802d1087fb6e0a7
PrasadKarunagoda <prasad@balcoo.com>
2021-09-08 22:34:01 +0530

    Add secure-web-app module

---
+++ secure-web-app/README.txt
@@ -0,0 +1,2 @@
+This project is based on following sprint.io guide:
+https://spring.io/guides/gs/securing-web/
---
+++ secure-web-app/src/test/java/prasad/research/securewebapp/SecureWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package prasad.research.securewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
